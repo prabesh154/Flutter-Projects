21b3fd0b4ce225b6b66cc5d8b8fbf315698c1278
prabesh <prabeshbizz@gmail.com>
2023-06-27 09:10:07 +0545

    Configuring Columns

--- quizapp/lib/questions_screen.dart
+++ quizapp/lib/questions_screen.dart
@@ -11,6 +11,21 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('Questions');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The question is ...'),
+          const SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 1'),
+          ),
+        ],
+      ),
+    );
   }
 }
