00ff8ca9eb6bc5cc4a46bb89795edafae8ab7a22
prabesh <prabeshbizz@gmail.com>
2023-08-09 22:01:48 +0545

    tabbar pinned using SliverAppBar

--- instaclone/lib/screens/profile_screen/profile_screen.dart
+++ instaclone/lib/screens/profile_screen/profile_screen.dart
@@ -1,55 +1,62 @@
-// import 'package:flutter/material.dart';
-// import 'package:instaclone/components/profile/profile_header.dart';
-// import 'package:instaclone/components/profile/profiletab_component.dart';
-// import 'package:instaclone/test_data.dart';
-
-// import '../../components/profile/custom_profile_appbar.dart';
-
-// class ProfileScreen extends StatelessWidget {
-//   const ProfileScreen({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return NestedScrollView(
-//       headerSliverBuilder: (context, index) {
-//         return [
-//           const CustomProfileAppBar(),
-//           const SliverToBoxAdapter(
-//             child: ProfileHeader(),
-//           ),
-//         ];
-//       },
-//       body: Column(
-//         children: [
-//           for (final post in postList) ProfileTabComponent(post: post),
-//         ],
-//       ),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:instaclone/components/profile/profile_header.dart';
-import 'package:instaclone/components/profile/profiletab_component.dart';
 import 'package:instaclone/test_data.dart';
 
 import '../../components/profile/custom_profile_appbar.dart';
+import '../../components/profile/profiletab_component.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return NestedScrollView(
-        headerSliverBuilder: (context, index) {
-          return [
-            const CustomProfileAppBar(),
-            const SliverToBoxAdapter(
-              child: ProfileHeader(),
-            ),
-          ];
-        },
-        body: ProfileTabComponent(
-          post: postList[0],
-        ));
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        body: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [
+              const CustomProfileAppBar(),
+              const SliverToBoxAdapter(
+                child: ProfileHeader(),
+              ),
+              const SliverAppBar(
+                expandedHeight: 50,
+                automaticallyImplyLeading: false,
+                backgroundColor: Colors.white,
+                pinned: true,
+                flexibleSpace: Column(
+                  children: [
+                    SizedBox(height: 5),
+                    TabBar(
+                      indicatorColor: Colors.black,
+                      tabs: [
+                        Tab(
+                          icon: Icon(
+                            Icons.category_sharp,
+                            color: Colors.black,
+                            size: 30,
+                          ),
+                        ),
+                        Tab(
+                          icon: Icon(
+                            Icons.photo_camera,
+                            color: Colors.black,
+                            size: 30,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ];
+          },
+          body: ProfileTabComponent(
+            post: postList[0],
+          ),
+        ),
+      ),
+    );
   }
 }
