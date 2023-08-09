9c854c6dce277ab6492cfcc25ac992baa77397ec
prabesh <prabeshbizz@gmail.com>
2023-08-09 22:02:48 +0545

    tabBar View and Grid View Implemetation

--- instaclone/lib/components/profile/profiletab_component.dart
+++ instaclone/lib/components/profile/profiletab_component.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../../models/post.dart';
 
 class ProfileTabComponent extends StatelessWidget {
@@ -8,61 +7,35 @@ class ProfileTabComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Column(
-        children: [
-          const TabBar(
-            indicatorColor: Colors.black,
-            tabs: [
-              Tab(
-                icon: Icon(
-                  Icons.category_sharp,
-                  color: Colors.black,
-                  size: 30,
-                ),
-              ),
-              Tab(
-                icon: Icon(
-                  Icons.photo_camera,
-                  color: Colors.black,
-                  size: 30,
-                ),
-              ),
-            ],
+    return TabBarView(
+      children: [
+        GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            mainAxisSpacing: 1,
+            crossAxisSpacing: 1,
           ),
-          Expanded(
-            child: TabBarView(
-              children: [
-                // First Tab: Categories
-                GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3, // Adjust the number of columns
-                    mainAxisSpacing: 1, // Add vertical space between images
-                    crossAxisSpacing: 1, // Add horizontal space between images
-                  ),
-                  itemCount: 3, // Replace with your actual item count
-                  itemBuilder: (context, index) {
-                    return Image.network(post.imageUrl);
-                  },
-                ),
-                // Second Tab: Photo Album
-                GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3, // Adjust the number of columns
-                    mainAxisSpacing: 1, // Add vertical space between images
-                    crossAxisSpacing: 1, // Add horizontal space between images
-                  ),
-                  itemCount: 3, // Replace with your actual item count
-                  itemBuilder: (context, index) {
-                    return Image.network(post.imageUrl);
-                  },
-                ),
-              ],
-            ),
+          itemCount: 3,
+          itemBuilder: (context, index) {
+            return Image.network(
+                'https://i.pinimg.com/originals/52/13/d0/5213d0ba3a0b84aa83d55ec157cf65e8.jpg');
+            // return Image.network(post.imageUrl);
+          },
+        ),
+        GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            mainAxisSpacing: 1,
+            crossAxisSpacing: 1,
           ),
-        ],
-      ),
+          itemCount: 3,
+          itemBuilder: (context, index) {
+            // return Image.network(post.imageUrl);
+            return Image.network(
+                'https://i.pinimg.com/originals/52/13/d0/5213d0ba3a0b84aa83d55ec157cf65e8.jpg');
+          },
+        ),
+      ],
     );
   }
 }
